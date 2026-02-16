40f90926081c63f798e7dd2132ae8396d3600311
Alexsvargas-CS <alexsvargascs@gmail.com>
2026-02-16 12:42:43 -0800

    relocated nodemoules and files to .gitignore

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,16 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 import express from "express";
+
+dotenv.config();
+
+const { MONGO_CONNECTION_STRING } = process.env;
+
+mongoose.set("debug", true);
+mongoose
+  .connect(MONGO_CONNECTION_STRING + "users") // connect to Db "users"
+  .catch((error) => console.log(error));
+  
 
 const app = express();
 const PORT = 8000;
